a697cb2e1dac6b5e79806f8ed6374e1ccec11d66
Felipe Carneiro <felipevilarcarneiro@outlook.com>
2026-02-10 18:21:31 -0300

    Implement modern word dictionary with search function
    Added a dictionary of modern words with their meanings and a function to search for them.

---
+++ main.py
@@ -0,0 +1,29 @@
+dict = {
+    "Cringe": "algo vergonhoso ou constrangedor",
+    "Stalkear": "investigar a vida de alguém online",
+    "Vdd": "abreviação da palavra |verdade|",
+    "Hater": "pessoa que está constantemente criticando os outros",
+    "Boila": "conjundo de fitas pequenas",
+    "Reentrância": "cavidade, depressão a uma superficie relativo ao seu nivel"
+        }
+
+def achar():
+    word = str(input("Digite uma palavra moderna que voce nao entende: \n 0 - Dicionario inteiro"))
+    print("\n")
+    
+    if word == "0":
+        for words in dict:
+            print(words+":")
+            print("É", dict[words])
+            print("\n")
+            
+    if word.capitalize() in dict:
+        print(word+":")
+        print("É", dict[word])
+        
+    else:
+        print("Tente novamente!")
+
+    achar()
+
+achar()
